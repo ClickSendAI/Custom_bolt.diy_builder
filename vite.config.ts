54e3c46ba70902c6ada55c18a6d76ed9785a0778
ClickSendAI <growthautomationsystem@gmail.com>
2026-05-31 12:57:57 -0700

    Update vite.config.ts
    Edit vite.config.ts: Change sourcemap: 'inline' to false

--- vite.config.ts
+++ vite.config.ts
@@ -93,7 +93,7 @@ export default defineConfig((config) => {
     },
     build: {
       target: 'esnext',
-      sourcemap: 'inline',
+      sourcemap: false,
     },
     plugins: [
       nodePolyfills({
